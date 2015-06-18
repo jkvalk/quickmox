424e58312a2884b2315ab66be0cc1e334cc3748a
J.K. Valk <jkvalk@mail.ru>
2015-06-18 15:24:15 +0300

    code formatting

--- lib/quickmox/guestlist.rb
+++ lib/quickmox/guestlist.rb
@@ -1,12 +1,12 @@
 class Guestlist < Array
 
-    def find(id)
-      self.each do |el|
-        if el.id.eql?(id.to_s)
-          return el
-        end
+  def find(id)
+    self.each do |el|
+      if el.id.eql?(id.to_s)
+        return el
       end
     end
+  end
 
   def scan
     self.each do |el|
